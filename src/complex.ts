3646971f20b77cb15128acf0a8ac85c9c6e4ee9e
Adrien Guesnel <adrienguesnel@gmail.com>
2020-05-21 13:30:46 +0200

    All components to publish first package

---
+++ src/complex.ts
@@ -0,0 +1,9 @@
+function modulo(a: number, b: number): number {
+  return a % b;
+}
+
+function square(a: number): number {
+  return Math.pow(a, 2);
+}
+
+export { modulo, square };
---
+++ src/simple.ts
@@ -0,0 +1,17 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function substract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+export { add, substract, multiply, divide };
